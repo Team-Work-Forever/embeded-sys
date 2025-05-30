b8d2c371e7e79f6ed49bfee025cfd447986a2588
David Braga <74202840+David-Ogrande@users.noreply.github.com>
2025-05-30 22:48:53 +0100

    Control arduino
    * add(control): initial structure of the control arduino
    
    * add(control): update structure of the control arduino

---
+++ control/src/logic/bluetoothCommandHandler.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "../components/infra_red.h"
+#include "../components/buzzer.h"
+
+class BluetoothCommandHandler
+{
+public:
+    static void handle(const String &command, Stream &bt, InfraRed &infraRedIn, InfraRed &infraRedOut, Buzzer &buzzer)
+    {
+        String cmd = command;
+        cmd.trim();
+
+        if (cmd == "BUZZER_ON")
+        {
+            buzzer.on();
+            bt.println("Buzzer ativado.");
+        }
+        else if (cmd == "BUZZER_OFF")
+        {
+            buzzer.off();
+            bt.println("Buzzer desativado.");
+        }
+        else
+        {
+            bt.println("Unknown command.");
+        }
+    }
+};
--- control/src/main.cpp
+++ control/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include "logic/park_manager.h"
 #include "logic/bluetoothCommandHandler.h"
 #include "components/infra_red.h"
 #include "components/buzzer.h"
-#include <LiquidCrystal.h>
+#include "./comn/bluetooth_transmitter.h"
+// #include <LiquidCrystal.h>
 
+SoftwareSerial BT(2, 3);
 BluetoothTransmitter bt(BT);
 
 #define INFRA_RED_IN_PIN A0
@@ -15,7 +16,7 @@ BluetoothTransmitter bt(BT);
 InfraRed infraRedIn(INFRA_RED_IN_PIN);
 InfraRed infraRedOut(INFRA_RED_OUT_PIN);
 Buzzer buzzer(BUZZER_PIN);
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int numCars = 0;
 
 void setup()
@@ -26,9 +27,9 @@ void setup()
   infraRedOut.init();
   buzzer.init();
 
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("Cars: 0   ");
+  // lcd.begin(16, 2);
+  // lcd.setCursor(0, 0);
+  // lcd.print("Cars: 0   ");
 
   BT.begin(9600);
   BT.println("Bluetooth ready. Send commands.");
@@ -36,19 +37,19 @@ void setup()
 
 void loop()
 {
-  bool inDetected = infraRedIn.isDetected();
-  if (inDetected && !lastInDetected)
-  {
-    BT.println("CAR IN");
-  }
-  lastInDetected = inDetected;
+  // bool inDetected = infraRedIn.isDetected();
+  // if (inDetected && !lastInDetected)
+  // {
+  //   BT.println("CAR IN");
+  // }
+  // lastInDetected = inDetected;
 
-  bool outDetected = infraRedOut.isDetected();
-  if (outDetected && !lastOutDetected)
-  {
-    BT.println("CAR OUT");
-  }
-  lastOutDetected = outDetected;
+  // bool outDetected = infraRedOut.isDetected();
+  // if (outDetected && !lastOutDetected)
+  // {
+  //   BT.println("CAR OUT");
+  // }
+  // lastOutDetected = outDetected;
 
   if (BT.available())
   {
@@ -57,15 +58,15 @@ void loop()
     if (command.startsWith("SET CAPACITY"))
     {
       int value = command.substring(13).toInt();
-      numCars = value;
-      lcd.setCursor(0, 0);
-      lcd.print("Cars: ");
-      lcd.print(numCars);
-      lcd.print("   ");
+      // numCars = value;
+      // lcd.setCursor(0, 0);
+      // lcd.print("Cars: ");
+      // lcd.print(numCars);
+      // lcd.print("   ");
     }
     else
     {
-      BluetoothCommandHandler::handle(command, manager, BT, infraRedIn, infraRedOut, buzzer);
+      BluetoothCommandHandler::handle(command, BT, infraRedIn, infraRedOut, buzzer);
     }
   }
   delay(150);
