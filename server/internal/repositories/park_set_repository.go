bfb24f7b7a8325ec61fe170563500767000ac56c
David-Ogrande <davidjosebraga@gmail.com>
2025-06-02 00:31:57 +0100

    refactor(server): allow to disconnect and connect without crash the program

--- server/internal/repositories/park_set_repository.go
+++ server/internal/repositories/park_set_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"log"
 	"server/internal/domain"
 	"server/pkg"
 
@@ -20,6 +21,8 @@ type (
 		UpdateLot(lot *domain.ParkLot) error
 		UpdateLotState(publicId string, state domain.ParkLotState) error
 		GetByMAC(mac string) (*domain.ParkSet, error)
+		GetLotsByParkSetID(parkSetID string) ([]domain.ParkLot, error)
+		DeleteReservationsBySlotId(slotId string) error
 	}
 )
 
@@ -31,24 +34,24 @@ func NewParkSetRepository(database pkg.Database) *ParkSetRepository {
 
 func (pr *ParkSetRepository) GetAllParkSets() ([]*domain.ParkSet, error) {
 	var parkSets []*domain.ParkSet
-
 	if err := pr.Context.Statement.Preload("Lots").Find(&parkSets).Error; err != nil {
 		return nil, err
 	}
-
 	return parkSets, nil
 }
 
 func (pr *ParkSetRepository) GetLotByPublicId(publicId string) (*domain.ParkLot, error) {
 	var lot domain.ParkLot
-	if err := pr.Context.Statement.Where("public_id = ?", publicId).First(&lot).Error; err != nil {
+	db := (*gorm.DB)(pr.Context)
+	if err := db.Where("public_id = ?", publicId).First(&lot).Error; err != nil {
 		return nil, err
 	}
 	return &lot, nil
 }
 
 func (pr *ParkSetRepository) UpdateLot(lot *domain.ParkLot) error {
-	return pr.Context.Statement.Save(lot).Error
+	db := (*gorm.DB)(pr.Context)
+	return db.Save(lot).Error
 }
 
 func (pr *ParkSetRepository) UpdateLotState(publicId string, state domain.ParkLotState) error {
@@ -60,9 +63,30 @@ func (pr *ParkSetRepository) UpdateLotState(publicId string, state domain.ParkLo
 
 func (pr *ParkSetRepository) GetByMAC(mac string) (*domain.ParkSet, error) {
 	var parkSet domain.ParkSet
-	err := pr.Context.Statement.Where("mac_address = ?", mac).First(&parkSet).Error
+	db := (*gorm.DB)(pr.Context)
+	err := db.Unscoped().Where("mac_address = ?", mac).First(&parkSet).Error
 	if err != nil {
 		return nil, err
 	}
 	return &parkSet, nil
+}
+
+func (pr *ParkSetRepository) GetLotsByParkSetID(parkSetID string) ([]domain.ParkLot, error) {
+	var lots []domain.ParkLot
+	db := (*gorm.DB)(pr.Context)
+	err := db.Unscoped().Where("park_set_id = ?", parkSetID).Find(&lots).Error
+	if err != nil {
+		return nil, err
+	}
+	return lots, nil
+}
+
+func (pr *ParkSetRepository) DeleteReservationsBySlotId(slotId string) error {
+	db := (*gorm.DB)(pr.Context)
+	if err := db.Where("slot_id = ?", slotId).Delete(&domain.Reserve{}).Error; err != nil {
+		log.Printf("Error deleting reservations for slot %s: %v", slotId, err)
+		return err
+	}
+	log.Printf("Reservations for slot %s deleted", slotId)
+	return nil
 }
